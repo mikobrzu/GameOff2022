5ed854fb88035fbd3d76f3fae6f16e80fea10ad8
mikobrzu <miko.brzuchalski@gmail.com>
2022-11-02 18:28:44 +0000

    Implementing player controller and input.

---
+++ GameOff2022-Project/Assets/Scripts/InputManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+
+    void Awake(){
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate(){
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void OnEnable(){
+        onFoot.Enable();
+    }
+
+    private void OnDisable(){
+        onFoot.Disable();
+    }
+}
